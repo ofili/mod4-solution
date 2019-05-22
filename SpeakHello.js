ee410cdae0f565efc18c9d63d3589cd9bae8cc55
Lewis <ofili@users.noreply.github.com>
2019-05-22 15:39:31 +0100

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -25,6 +25,6 @@
     // 'helloSpeaker' on the global scope as well.
     // See Lecture 52, part 2
     // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-
-    window.helloSpeaker = helloSpeaker;
+    window.speak = helloSpeaker.speak
+    window.helloSpeaker = helloSpeaker
 })(window);
